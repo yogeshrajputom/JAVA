3aab5d31483158637dbbfd9b47b7b324891c577e
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-03-05 10:02:44 +0000

    5/3/24

---
+++ java/javaLoop/PrimeNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * PrimeNumber
+ */
+public class PrimeNumber {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a any Number :");
+        int a = sc.nextInt();
+
+    }
+}
