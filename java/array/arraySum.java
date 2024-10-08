d7b9ec08b8242b678e85ac317057ce837b727298
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-08 16:35:41 +0000

    8/10/2024

---
+++ java/array/arraySum.java
@@ -0,0 +1,13 @@
+public class arraySum {
+    public static void main (String [] args){
+
+        int arrs[] = {10,24,25,85,67,47,53,57};
+    //     int sum =0;
+    //     for (int ar: arrs ){
+    //         sum+=ar;
+    //     }
+    //     System.out.println(sum);
+    // }
+    
+    int min = 0; 
+}
