c0ebe0186437afae6ca206c269898825bda83470
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-08 15:04:35 +0000

    8/10/2024

--- java/OperatorsNew.java
+++ java/OperatorsNew.java
@@ -1,3 +1,16 @@
 public class OperatorsNew {
+    public static void main(String[] args) {
+        int a=4;
+        int b=3;
+        // Operators
+        System.out.println(a+b);//Addition
+        System.out.println(a*b);//Multiplication
+        System.out.println(a-b);//Subtraction
+        double c=(double)a/b;
+        System.out.println(c);//Division
+        //Modulo
+        System.out.println(a%b);
+
+    }
     
 }
