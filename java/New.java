760234fc718ca37f640481a18c39918541b58669
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-08 07:03:40 +0000

    8/10/2024

---
+++ java/New.java
@@ -0,0 +1,13 @@
+public class New {
+    public static void main(String[] args) {
+        System.out.println("Hello !");
+
+        // veriaval
+        int a =228;
+        short b = (short) a;
+        byte c = (byte) a;
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+}
