8927f4b40c1a04e4672a659d34e6b906fbb722a4
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-08 15:04:20 +0000

    8/10/2024

---
+++ java/array/array.java
@@ -0,0 +1,22 @@
+public class array {
+    public static void main(String[] args) {
+        
+        int arg[]; //declaration
+
+        arg=new int[5];  //allocation
+        
+            //  int arg[]=new int[5]   one line array declaration
+        arg[0]=6;
+        arg[1]=3;
+        arg[2]=6;
+        arg[3]=8;
+        arg[4]=6;
+        System.out.println(arg[0]);
+        System.out.println(arg[1]);
+        System.out.println(arg[2]);
+        System.out.println(arg[3]);
+        System.out.println(arg[4]);
+        
+    }
+    
+}
