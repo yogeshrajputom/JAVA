a43d2506481eb184735a60e3cc9cc6c036acd8d9
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-15 12:06:53 +0000

    15/10/2024

---
+++ java/array/arr2d.java
@@ -0,0 +1,25 @@
+public class arr2d {
+    public static void main (String []args){
+        int arrs[][]={
+        {24,54,84},
+        {65,58,26},
+        {45,52,69},
+        {57,65,39}};
+
+
+        System.out.println(arrs[3][2]);
+        // int arr[][]= new int[4][3];
+        // arr[0][0]=52;
+        // arr[0][1]=84;
+        // arr[0][2]=74;
+        // arr[1][0]=54;
+        // arr[1][1]=82;
+        // arr[1][2]=79; 
+        // arr[2][0]=55;
+        // arr[2][1]=87;
+        // arr[2][2]=73;
+        // System.out.println(arr[0][0]);
+       
+    
+    }
+}
