e46bf837e8e5052b047c7ec5501843977f81ae08
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-16 11:28:06 +0000

    16/10/2024

---
+++ java/A.java
@@ -0,0 +1,12 @@
+public class A {
+    public static void main(String args[]) {
+        System.out.println("Hello g");
+        System.out.println("Yoge");
+    int a, b;
+        a = 5;
+        b = 8;
+        int sume = a + b;
+        System.out.println(sume);
+    }
+}
+
