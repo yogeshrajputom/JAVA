95e0aabb6552592394eb280a9b895f261b40489d
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-17 13:29:56 +0000

    17/10/2024

---
+++ java/loops/loops/Table.java
@@ -0,0 +1,22 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        System.out.println("Enter a Value :");
+        Scanner sc = new Scanner(System.in);
+        int a ,sum=0;
+        a = sc.nextInt();
+        for(int i =1;i<=a;i++){
+            sum=sum+i;  
+        }
+        System.out.println(sum);
+                            // for(int i=1;i<=10;i++)
+                            // for (int i = 10; i >= 1; i--) {
+
+                            //     System.out.println(a + "X" + i + "=" + a * i);
+        }
+    }
+
+}
