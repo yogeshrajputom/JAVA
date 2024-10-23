3316ccac061442a751d145a1a9c37a4ac61fddbd
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-23 08:08:15 +0000

    23/10/2024

---
+++ java/patterns/piramid.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class piramid {
+    public static void main(String[] args) {
+        System.out.println("Enter a No.:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+       
+    }
+}
